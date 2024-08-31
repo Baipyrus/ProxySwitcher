67760c7bfc244cbb91ca242af565dc73e8bfaab8
Baipyrus <simon.waltemathe@gmail.com>
2024-08-31 18:13:27 +0200

    extend and format config types

--- util/types.go
+++ util/types.go
@@ -1,14 +1,21 @@
 package util
 
 type Config struct {
-	Name  string    `json:"name"`
-	Cmd   string    `json:"cmd"`
-	Set   []Variant `json:"set,omitempty"`
-	Unset []Variant `json:"unset,omitempty"`
+	Name  string     `json:"name"`
+	Cmd   string     `json:"cmd"`
+	Set   []*Variant `json:"set,omitempty"`
+	Unset []*Variant `json:"unset,omitempty"`
 }
+
+type Type string
+
+const (
+	TEXT     Type = "text"
+	VARIABLE Type = "variable"
+)
 
 type Variant struct {
 	Arguments []string `json:"args"`
-	Type      string   `json:"type,omitempty"`
+	Type      Type     `json:"type,omitempty"`
 	Equator   string   `json:"equator,omitempty"`
 }
