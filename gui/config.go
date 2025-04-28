4b3f584fd9c76ca90dec496e1f3ecd2da9c04586
Baipyrus <simon.waltemathe@gmail.com>
2025-04-28 21:21:59 +0200

    setup tree-table view and config parsing

--- gui/config.go
+++ gui/config.go
@@ -3,7 +3,11 @@
 package gui
 
 import (
+	"slices"
+	"strings"
+
 	g "github.com/AllenDang/giu"
+	"github.com/Baipyrus/ProxySwitcher/util"
 )
 
 var (
@@ -33,10 +37,64 @@ func configWindow(cfgPath string) {
 		g.Align(g.AlignCenter).To(
 			g.Label("Configurations"),
 		),
+		g.TreeTable().
+			Columns(g.TableColumn("Categories")).
+			Rows(generateTree(cfgPath)...).
+			Size(g.Auto, g.Auto),
 	)
 }
 
+func splitPath(path string) [][]string {
+	parts := strings.Split(path, "-")
+	for i := range parts {
+		parts[i] = strings.TrimSpace(parts[i])
+	}
 
+	var result [][]string
+	for i := range parts {
+		// Concatenate all paths up to current
+		result = append(result, parts[:i+1])
+	}
+
+	return result
+}
+
+func parseCategories(cfgPath string) map[string][]string {
+	// Read configs, split into categories based on name
+	cats := make(map[string][]string)
+	configs, _ := util.ReadConfigs(cfgPath)
+
+	for _, cfg := range configs {
+		for _, levels := range splitPath(cfg.Name) {
+			// Extract hierarchy and item name
+			parent := strings.Join(levels[:len(levels)-1], " - ")
+			item := levels[len(levels)-1]
+
+			// Initialize category slice if empty
+			if _, ok := cats[parent]; !ok {
+				cats[parent] = []string{item}
+				continue
+			}
+
+			// Ignore duplicate entries
+			if slices.Contains(cats[parent], item) {
+				continue
+			}
+
+			cats[parent] = append(cats[parent], item)
+		}
+	}
+
+	return cats
+}
+
+func generateTree(cfgPath string) (tree []*g.TreeTableRowWidget) {
+	cats := parseCategories(cfgPath)
+
+	// Map tree by recursively getting nodes from categories
+
+	return tree
+}
 
 func Config(cfgPath string) {
 	title := "Proxy Switcher - Config"
