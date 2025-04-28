66f96ad841118a9d47ac76af8b66c7edb7a3015a
Baipyrus <simon.waltemathe@gmail.com>
2025-04-28 18:20:25 +0200

    feat: basic proxy server setting gui

--- gui/config.go
+++ gui/config.go
@@ -6,12 +6,36 @@ import (
 	g "github.com/AllenDang/giu"
 )
 
+var (
+	protocols = []string{"http", "https"}
+	selection int32
+	host      string
+	port      int32
+)
+
+func config_window() {
+	g.SingleWindow().Layout(
+		g.Align(g.AlignCenter).To(
+			g.Label("Proxy Server"),
+		),
+		g.Row(
+			g.Label("Protocol:"),
+			g.Combo("", protocols[selection], protocols, &selection),
+		),
+		g.Row(
+			g.Label("Host:    "),
+			g.InputText(&host),
+		),
+		g.Row(
+			g.Label("Port:    "),
+			g.InputInt(&port),
+		),
+	)
+}
+
 func Config() {
 	title := "Proxy Switcher - Config"
 	wnd := g.NewMasterWindow(title, 400, 200, g.MasterWindowFlagsNotResizable)
-	wnd.Run(func() {
-		g.SingleWindow().Layout(
-			g.Label("TODO: Add basic proxy URL configuration"),
-		)
-	})
+
+	wnd.Run(config_window)
 }
